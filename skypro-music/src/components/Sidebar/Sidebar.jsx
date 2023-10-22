17b68aab1a78948794e64490410a064873f637f6
karnaukhoff <n4echduf@gmail.com>
2023-10-22 22:22:39 +0300

    готово

--- skypro-music/src/components/Sidebar/Sidebar.jsx
+++ skypro-music/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import "./Sidebar.css";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css';
+import * as S from "./Sidebar.styles"
 import React, { useState } from "react";
 
 function Sidebar() {
@@ -9,83 +9,71 @@ function Sidebar() {
     setLoading(false);
   }, 3000);
   return (
-    <div className="main__sidebar sidebar">
-      <div className="sidebar__personal">
-        <p className="sidebar__personal-name">Sergey.Ivanov</p>
-        <div className="sidebar__icon">
+    <S.MainSidebar>
+      <S.SidebarPersonal>
+        <S.SidebarPersonalName>Sergey.Ivanov</S.SidebarPersonalName>
+        <S.SidebarIcon>
           <svg alt="logout">
             <use xlinkHref="img/icon/sprite.svg#logout"></use>
           </svg>
-        </div>
-      </div>
-      <div className="sidebar__block">
-        <div className="sidebar__list">
-          <div className="sidebar__item">
-            <SkeletonTheme
-                baseColor="#313131"
-                highlightColor="#fff"
-                height={150}
-                width={250}
-              >
-              {isLoading ? (
-                  <Skeleton />
-                ) : (
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                    )
-                }
-            </SkeletonTheme>
-          </div>
-          <div className="sidebar__item">
+        </S.SidebarIcon>
+      </S.SidebarPersonal>
+      <S.SidebarBlock>
+        <S.SidebarList>
+          <S.SidebarItem>
               <SkeletonTheme
-                baseColor="#313131"
-                highlightColor="#fff"
-                height={150}
-                width={250}
-              >
-              {isLoading ? (
-                  <Skeleton />
-                ) : (
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                    )
-                }
-            </SkeletonTheme>
-          </div>
-          <div className="sidebar__item">
+                  baseColor="#313131"
+                  highlightColor="#fff"
+                  height={150}
+                  width={250}
+                >
+                {isLoading ? (
+                    <Skeleton />
+                  ) : (
+                      <S.SidebarLink href="/#">
+                        <S.SidebarImg src="img/playlist01.png" alt="day's playlist" />
+                      </S.SidebarLink>
+                      )
+                  }
+              </SkeletonTheme>
+          </S.SidebarItem>
+          <S.SidebarItem>
               <SkeletonTheme
-                baseColor="#313131"
-                highlightColor="#fff"
-                height={150}
-                width={250}
-              >
-              {isLoading ? (
-                  <Skeleton />
-                ) : (
-                  <a className="sidebar__link" href="#">
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                    )
-                }
-            </SkeletonTheme>
-          </div>
-        </div>
-      </div>
-    </div>
+                  baseColor="#313131"
+                  highlightColor="#fff"
+                  height={150}
+                  width={250}
+                >
+                {isLoading ? (
+                    <Skeleton />
+                  ) : (
+                      <S.SidebarLink href="/#">
+                        <S.SidebarImg src="img/playlist02.png" alt="day's playlist" />
+                      </S.SidebarLink>
+                      )
+                  }
+              </SkeletonTheme>
+          </S.SidebarItem>
+          <S.SidebarItem>
+              <SkeletonTheme
+                  baseColor="#313131"
+                  highlightColor="#fff"
+                  height={150}
+                  width={250}
+                >
+                {isLoading ? (
+                    <Skeleton />
+                  ) : (
+                      <S.SidebarLink href="/#">
+                        <S.SidebarImg src="img/playlist01.png" alt="day's playlist" />
+                      </S.SidebarLink>
+                      )
+                  }
+              </SkeletonTheme>
+          </S.SidebarItem>  
+        </S.SidebarList>
+      </S.SidebarBlock>
+    </S.MainSidebar>
   );
 }
 export default Sidebar;
--- skypro-music/src/components/TreckList/TreckList.jsx
+++ skypro-music/src/components/TreckList/TreckList.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import "./TreckList.css";
+//import "./TreckList.css";
+import * as S from "./TreckList.styles"
 import Track from "../components";
 
 function treckList() {
   return (
-    <div className="centerblock__content">
-      <div className="content__title playlist-title">
-        <div className="playlist-title__col col01">Трек</div>
-        <div className="playlist-title__col col02">ИСПОЛНИТЕЛЬ</div>
-        <div className="playlist-title__col col03">АЛЬБОМ</div>
-        <div className="playlist-title__col col04">
-          <svg className="playlist-title__svg" alt="time">
-            <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
-          </svg>
-        </div>
-      </div>
-      <div className="content__playlist playlist">
+    <S.CenterblockContent>
+      <S.ContentTitle>
+        <S.PlaylistTitleCol>
+          <S.Col01>Трек</S.Col01>
+        </S.PlaylistTitleCol>
+        <S.PlaylistTitleCol>
+          <S.Col02>ИСПОЛНИТЕЛЬ</S.Col02>
+        </S.PlaylistTitleCol>
+        <S.PlaylistTitleCol>
+          <S.Col03>АЛЬБОМ</S.Col03>
+        </S.PlaylistTitleCol>
+        <S.PlaylistTitleCol>
+          <S.Col04>
+            <S.PlaylistTitleSvg alt="time">
+              <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+            </S.PlaylistTitleSvg>
+          </S.Col04>
+        </S.PlaylistTitleCol>
+      </S.ContentTitle>
+      <S.ContentPlaylistItems>
         <Track trackName="Guilt" trackAuthor="Nero" album="Weloome Reality" trackTime="4.44"/>
         <Track trackName="Elektro" trackAuthor="Dynoro, Outwork, Mr. Gee" album="Elektro" trackTime="2.22"/>
         <Track trackName="I’m Fire" trackAuthor="Ali Bakgor" album="I’m Fire" trackTime="2.22 "/>
@@ -26,8 +35,8 @@ function treckList() {
         <Track trackName="Knives n Cherries" trackAuthor="minthaze" album="Captivating" trackTime="1.48"/>
         <Track trackName="How Deep Is Your Love" trackAuthor="Calvin Harris, Disciples" album="How Deep Is Your Love" trackTime="3.32"/>
         <Track trackName="Morena" trackAuthor="Tom Boxer" album="Soundz Made in Romania" trackTime="3.36"/>
-      </div>
-    </div>
+        </S.ContentPlaylistItems>
+      </S.CenterblockContent>
   );
 }
 export default treckList;
